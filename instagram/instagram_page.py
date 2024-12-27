f93a444caa83b5257a306778cbd88ed63e9b2376
Sandeep <talapally.kumar@oracle.com>
2024-12-28 01:14:14 +0530

    added new authorize link

--- instagram/instagram_page.py
+++ instagram/instagram_page.py
@@ -8,10 +8,14 @@ from instagram.instagram_utils import is_paid_subscriber, is_authorized_subscrib
 
 def handle_instagram_login():
     id = st.session_state.get("user_id", "")
-    instagram_url = (
-        f"https://www.instagram.com/oauth/authorize?enable_fb_login=0&force_authentication=1&client_id=581334411142953&redirect_uri=https://owlit-backend.vercel.app/instagram_redirect"
-        f"&response_type=code&scope=instagram_business_basic%2Cinstagram_business_manage_messages%2Cinstagram_business_manage_comments%2Cinstagram_business_content_publish&state=id%3D{id}"
-    )
+    # instagram_url = (
+    #     f"https://www.instagram.com/oauth/authorize?enable_fb_login=0&force_authentication=1&client_id=581334411142953&redirect_uri=https://owlit-backend.vercel.app/instagram_redirect"
+    #     f"&response_type=code&scope=instagram_business_basic%2Cinstagram_business_manage_messages%2Cinstagram_business_manage_comments%2Cinstagram_business_content_publish&state=id%3D{id}"
+    # )
+    instagram_url = (f"https://www.instagram.com/oauth/authorize?enable_fb_login=0&force_authentication=1&client_id"
+                     f"=581334411142953&redirect_uri=https://api.owlit.in/instagram_redirect&response_type=code&scope"
+                     f"=instagram_business_basic%2Cinstagram_business_manage_messages"
+                     f"%2Cinstagram_business_manage_comments%2Cinstagram_business_content_publish&state=id%3D{id}")
 
     # Display a clickable button-like link
     st.markdown(
